432d8727c5c27bc9d2248d06c30d91ccf01a8d35
Ming-Yen Liu <42059426+thomas87920386@users.noreply.github.com>
2024-10-18 21:55:46 +0800

    Add files via upload

---
+++ IMU/gyroscope.ino
@@ -0,0 +1,72 @@
+#include <Wire.h>
+float RateRoll, RatePitch, RateYaw;
+float RateCalibrationRoll, RateCalibrationPitch, RateCalibrationYaw;
+int RateCalibrationNumber;
+
+void gyro_signals(void) {
+  Wire.beginTransmission(0x68);
+  Wire.write(0x1A);
+  Wire.write(0x05);
+  Wire.endTransmission();
+
+  Wire.beginTransmission(0x68);
+  Wire.write(0x1B);
+  Wire.write(0x8);
+  Wire.endTransmission();
+
+  Wire.beginTransmission(0x68);
+  Wire.write(0x43);
+  Wire.endTransmission();
+  Wire.requestFrom(0x68,6);
+
+  int16_t GyroX = Wire.read()<<8 | Wire.read();
+  int16_t GyroY = Wire.read()<<8 | Wire.read();
+  int16_t GyroZ = Wire.read()<<8 | Wire.read();
+
+  RateRoll = (float)GyroX/65.5;
+  RatePitch = (float)GyroY/65.5;
+  RateYaw = (float)GyroZ/65.5;
+}
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(57600);
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH);
+
+  Wire.setClock(400000);
+  Wire.begin();
+  delay(250);
+
+  Wire.beginTransmission(0x68);
+  Wire.write(0x6B);
+  Wire.write(0x00);
+  Wire.endTransmission();
+
+  for(RateCalibrationNumber=0; RateCalibrationNumber<2000; RateCalibrationNumber++)
+  {
+    gyro_signals();
+    RateCalibrationRoll += RateRoll;
+    RateCalibrationPitch += RatePitch;
+    RateCalibrationYaw += RateYaw;
+    delay(1);
+  }
+  RateCalibrationRoll /= 2000;
+  RateCalibrationPitch /= 2000;
+  RateCalibrationYaw /= 2000;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  gyro_signals();
+  RateRoll -= RateCalibrationRoll;
+  RatePitch -= RateCalibrationPitch;
+  RateYaw -= RateCalibrationYaw;
+  
+  Serial.print("Roll rate [*/s] = ");
+  Serial.println(RateRoll);
+  Serial.print("Pitch rate [*/s] = ");
+  Serial.println(RatePitch);
+  Serial.print("Yaw rate [*/s] = ");
+  Serial.println(RateYaw);
+  delay(50);
+}
